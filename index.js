13182750d3ac7804b587c2631fc46f5caeecf9af
Angelo220798 <106038476+Angelo220798@users.noreply.github.com>
2022-07-10 15:28:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ AOS destroller
           ${prefix}spam  Hace 200+ pings
           ${prefix}purge     Hace purge
           ${prefix}deleteroles Elimina todos los roles
-          ${prefix}auto   Elimina y crea canales nuevos ES BETA
           ${prefix}superbanall  Banea a todos mas rapido USARLO EN +100 members server
           ${prefix}icono  URL de un imagen
         `.italic.bold)
