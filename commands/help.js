efbeaa98a0e330509f7c2514736692d93c5311ef
Angelo220798 <106038476+Angelo220798@users.noreply.github.com>
2022-07-12 21:18:08 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
     let spamMessage = args.slice(0).join(" ");
     message.delete();
 
-        message.channel.sendMessage("https://cdn.discordapp.com/attachments/975547256373653514/996182395881537666/Screenshot_2022-07-11_18.32.37.png", spamMessage)
+        message.channel.sendMessage("https://cdn.discordapp.com/attachments/975547256373653514/996586090347630642/unknown.png", spamMessage)
 
     }
 
